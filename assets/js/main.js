2277c85dcd3be472aaec2e358582404bc59ea980
massimocolella02 <massimo-colella@outlook.it>
2023-04-20 12:06:13 +0200

    creazione html statico

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ]
+      }
+    }
+  }).mount('#app')
